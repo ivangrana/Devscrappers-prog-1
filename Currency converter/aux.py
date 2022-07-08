ef668b4518211a7df4277e519fcacb31903841d6
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2022-07-08 18:27:28 -0300

    Update aux.py

--- Currency converter/aux.py
+++ Currency converter/aux.py
@@ -1,20 +1,11 @@
-links = {'0':'https://www.x-rates.com/calculator/?from=BRL&to=USD&amount=1',
-         '1':'https://www.x-rates.com/calculator/?from=USD&to=BRL&amount=1',
-         '2':'https://www.x-rates.com/calculator/?from=BRL&to=EUR&amount=1',
-         '3':'https://www.x-rates.com/calculator/?from=EUR&to=BRL&amount=1',
-         '4':'https://www.x-rates.com/calculator/?from=BRL&to=GBP&amount=1',
-         '5':'https://www.x-rates.com/calculator/?from=BRL&to=ARS&amount=1',
-         '6':'https://www.x-rates.com/calculator/?from=ARS&to=BRL&amount=1',
-         '7':'https://www.x-rates.com/calculator/?from=USD&to=ARS&amount=1',
-         '8':'https://www.x-rates.com/calculator/?from=ARS&to=USD&amount=1'}
+links = ['https://www.x-rates.com/calculator/?from=BRL&to=USD&amount=1',
+         'https://www.x-rates.com/calculator/?from=USD&to=BRL&amount=1',
+         'https://www.x-rates.com/calculator/?from=BRL&to=EUR&amount=1',
+         'https://www.x-rates.com/calculator/?from=EUR&to=BRL&amount=1',
+         'https://www.x-rates.com/calculator/?from=BRL&to=GBP&amount=1',
+         'https://www.x-rates.com/calculator/?from=BRL&to=ARS&amount=1',
+         'https://www.x-rates.com/calculator/?from=ARS&to=BRL&amount=1',
+         'https://www.x-rates.com/calculator/?from=BRL&to=ARS&amount=1'
+         'https://www.x-rates.com/calculator/?from=USD&to=ARS&amount=1',
+         'https://www.x-rates.com/calculator/?from=ARS&to=USD&amount=1']
 
-par = [['BRL','USD'],
-       ['USD','BRL'],
-       ['BRL','EUR'],
-       ['EUR','BRL'],
-       ['BRL','GBP'],
-       ['BRL','ARS'],
-       ['ARS','BRL'],
-       ['USD','ARS'],
-       ['ARS','USD']]
-       
