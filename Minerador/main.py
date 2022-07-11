b243d4075caa8d385df64fccdf9195f5501e9242
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2022-07-11 01:50:33 -0300

    Update main.py

--- Minerador/main.py
+++ Minerador/main.py
@@ -5,6 +5,10 @@ from selenium.webdriver import ActionChains
 import requests
 from bs4 import BeautifulSoup
 import random
+import time
+import smtplib,getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # def raspador(filename):
 #     filename = 'exemplo.png'
@@ -59,19 +63,45 @@ def captura_tela(item):
     
     name_news = str(random.randint(10,10000)) + ".png"
     driver.save_full_page_screenshot(name_news)
-    driver.close()
+    #driver.close()
 
-
+def send_mail(new_list):
+    mail_content = ' '
+    for k in new_list:
+        mail_content += k
+        mail_content += '\n'
+        
+#Senha e email do remetente
+    remetente = 'devscrappers@outlook.com'
+    senha = '4hkbgp3r'
+    destinatario = 'ivan.grana@icen.ufpa.br'
+#MIME
+    message = MIMEMultipart()
+    message['From'] = remetente
+    message['To'] = destinatario
+    message['Subject'] = 'links raspados'   #Assunto do email
+#corpo e anexos
+    message.attach(MIMEText(mail_content, 'plain'))
+#Criação da sessão SMTP
+    s = smtplib.SMTP('smtp.office365.com', 587) #porta 587 do hotmail
+    s.starttls() #TLS
+    s.login(remetente, senha) #fazendo o login...
+    text = message.as_string()
+    s.sendmail(remetente, destinatario, text)
+    s.quit()
+    print('Email enviado')
+    
 #if __name__ == '__main__':
 
 def main(k):
     raspador_palavras_chave(k,urls[1])
-    raspador_palavras_chave2(k,urls[0])
+    #raspador_palavras_chave2(k,urls[0])
+    #raspador_palavras_chave(k,urls[2])
     new_list = list(dict.fromkeys(links_raspados))
     if 'https://markets.businessinsider.comhttps://www.insider.com/news' in new_list:
      new_list.remove('https://markets.businessinsider.comhttps://www.insider.com/news')
     sorted(new_list)
-    print(new_list)
-    for item in new_list:
-     captura_tela(item)
-
+    send_mail(new_list)
+    #print(new_list)
+    #for item in new_list:
+     #captura_tela(item)
